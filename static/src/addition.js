440c81cdf5c2f1d959e4cd1d5b57b0158c43b4ac
kevin <kevinwong88@gmail.com>
2017-07-19 18:22:02 -0700

    Updated docs

--- static/src/addition.js
+++ static/src/addition.js
@@ -31,9 +31,20 @@ var service = new Vue({
     result: "0.0"
   },
   methods: {
+    /* Since the callback is actually moving the data somewhere where it can be
+       used by this instance, it has to be its own instance method.
+
+       Sets `result` to the `response` attribute of the object sent by the
+       Python function addition.addition_response()
+
+       :param data: The JSON data returned by getJSON()
+    */
     getSumCallback: function(data){
         this.result = data['response'];
     },
+
+    /* Calls the `addition` service from the exposed API
+    */
     getSum: function() {
         var params = '?' + 'number1=' + this.number1 + '&' + 'number2=' + this.number2;
         getJSON(api + params, this.getSumCallback)
